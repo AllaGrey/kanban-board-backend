815fe68bb36333d67919fa0afcca34dfaf92e99a
AllaGrey <111253692+AllaGrey@users.noreply.github.com>
2024-05-16 13:30:05 +0300

    fix routes

--- dist/server.js
+++ dist/server.js
@@ -1,5 +1,22 @@
 "use strict";
-const app = require("./app");
-app.listen(3001, () => {
-    console.log("Server is running on port 3001");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = __importDefault(require("./app"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const { PORT, MONGO_URL } = process.env;
+app_1.default.listen(PORT || 3000, () => {
+    console.log(`Server is running on port ${PORT}`);
+    if (MONGO_URL) {
+        mongoose_1.default
+            .connect(MONGO_URL, {})
+            .then(() => console.log("Connected to MongoDB"))
+            .catch((error) => {
+            console.log(error.message);
+            process.exit(1);
+        });
+    }
 });
