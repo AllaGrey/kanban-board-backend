90f011556ad2b213e41935331291ed38ba703b58
AllaGrey <111253692+AllaGrey@users.noreply.github.com>
2024-05-19 11:43:06 +0300

    Update server.ts

--- dist/server.js
+++ dist/server.js
@@ -4,18 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const { PORT } = process.env;
+const { PORT, MONGODB_URI } = process.env;
 app_1.default.listen(PORT || 3000, () => {
     console.log(`Server is running on port ${PORT}`);
-    // if (MONGODB_URI) {
-    //   mongoose
-    //     .connect(MONGODB_URI, {})
-    //     .then(() => console.log("Connected to MongoDB"))
-    //     .catch((error: Error) => {
-    //       console.log(error.message);
-    //       process.exit(1);
-    //     });
-    // }
+    if (MONGODB_URI) {
+        mongoose_1.default
+            .connect(MONGODB_URI, {})
+            .then(() => console.log("Connected to MongoDB"))
+            .catch((error) => {
+            console.log(error.message);
+            process.exit(1);
+        });
+    }
 });
--- server.ts
+++ server.ts
@@ -5,18 +5,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const { PORT } = process.env;
+const { PORT, MONGODB_URI } = process.env;
 
 app.listen(PORT || 3000, () => {
   console.log(`Server is running on port ${PORT}`);
 
-  // if (MONGODB_URI) {
-  //   mongoose
-  //     .connect(MONGODB_URI, {})
-  //     .then(() => console.log("Connected to MongoDB"))
-  //     .catch((error: Error) => {
-  //       console.log(error.message);
-  //       process.exit(1);
-  //     });
-  // }
+  if (MONGODB_URI) {
+    mongoose
+      .connect(MONGODB_URI, {})
+      .then(() => console.log("Connected to MongoDB"))
+      .catch((error: Error) => {
+        console.log(error.message);
+        process.exit(1);
+      });
+  }
 }) as Server;
